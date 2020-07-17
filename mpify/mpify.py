738f923ff0bb99c468ef275347d2a80a733aacff
Phillip Chu <philtrade@winphil.net>
2020-07-18 02:55:10 +0800

    Overhaul import mechanism:  user can pass in a multi-line string of python import syntax via 'imports=' parameter.
    Also added 'need="string of object names"', to pass in other thingies in the caller's global space.

--- mpify/mpify.py
+++ mpify/mpify.py
@@ -1,73 +1,55 @@
-import os, sys, inspect, multiprocess as mp
+import os, sys, re, inspect, multiprocess as mp
 from typing import Callable
 from contextlib import AbstractContextManager, nullcontext
 import torch
 '''
-TODO: if in IPython, user defined local functions can be obtained, thus exportable to child process via kwargss:
-     fs = [ f for k,f in shell.user_ns.items() 
-        if type(f) == types.FunctionType and not k.startswith('_') and k == f.__name__ and f.__module__ == '__main__' ]
-
-    Can imported modules lines be discovered and sent over as well?
-    - yes, if in IPython, examine shell.user_ns dict, for all "_i{n}", look for:
-      "from x import y" and "import x as y", build the list and pass to contextualize.
-
-    The following seems to work in the target function of Process():
-    import importlib
-
-    # import torch
-    globals()['torch'] = importlib.import_module('torch')
-
-    # import torch.distributed as dist
-    globals()['dist' ] = importlib.import_module('torch.distributed')
-
+TODO:
 '''
-__all__ = ['import_star', 'ranch', 'TorchDDPCtx', 'in_torchddp', 'ddp_rank', 'ddp_worldsize']
+__all__ = ['import_star', 'global_imports', 'ranch', 'TorchDDPCtx', 'in_torchddp', 'ddp_rank', 'ddp_worldsize']
 
-def import_star(modules:[str]=[], g:dict=None):
-    "Apply `from module import '*'` into caller's frame from a list of modules."
-    cf = inspect.currentframe()
-    if g is None: g = cf.f_back.f_globals
-    try:
-        for mod in modules:
-            try:
-                m = __import__(mod, fromlist=['*'])
-                to_import = {}
-                for name in getattr(m, "__all__", [n for n in dir(m) if not n.startswith('_')]):
-                    to_import[name] = getattr(m, name)
-                g.update(to_import)
-            except Exception as e: raise ImportError(f"Failed to import module {mod}") from e
-    finally: del cf   # per https://docs.python.org/3/library/inspect.html#the-interpreter-stack
+#  - globals() doesn't necessarily return the '__main__' global scope when inside package function,
+#    thus we use sys.modules['__main__'].__dict__.
+#  - to access caller's globals, see https://docs.python.org/3/library/inspect.html#the-interpreter-stack
 
-def _contextualize(i:int, nprocs:int, fn:Callable, cm:AbstractContextManager, l=None, env:dict={}):
+def import_star(modules:[str], g:dict=None):
+    if g is None: import sys
+    global_imports([f"from {m} import *" for m in modules], g or sys.modules['__main__'].__dict__)
+
+def global_imports(imports:[str], globals:dict):
+    '''
+    Parse each import instruction in 'imports', and import each item using __import__(x, fromlist=[non-empty]).
+    fromlist=[''], the non-empty list, means 'import X.Y' will return leaf module Y instead of top module X.
+    '''
+    pat = re.compile(r'^\s*?(?:from\s+?(\S+?)\s+?)?import\s+?(.+)$')
+    pat_as = re.compile(r'^\s*?(\S+?)(?:\s*?as\s+?(\S+?))?\s*$')
+    for parsed in filter(lambda p:p,[pat.match(i) for i in imports]):
+        (from_, imp_)  = parsed.groups()
+        imps = imp_.split(',')
+
+        # If the import is "from X import <spec>", all specs will be from from_mod
+        from_mod = __import__(from_, fromlist=['']) if from_ else None
+
+        for name in imps: # each comma-separated item in import a, b, x as y
+            (x, y) = pat_as.match(name).groups()
+            if y is None: y=x
+            if x == '*': # starred import needs 'from'
+                assert from_, SyntaxError(f"Missing 'from <module> before 'import *': {parsed.string}")
+                importables = getattr(from_mod, "__all__", [n for n in dir(from_mod) if not n.startswith('_')])
+                for o in importables: globals[o] = getattr(from_mod, o)
+            else: # If the line doesn't begin with 'from', each 'x' is a new module to import.
+                globals[y] = getattr(from_mod, x) if from_ else __import__(x, fromlist=[''])
+
+def _contextualize(i:int, nprocs:int, fn:Callable, cm:AbstractContextManager, l=None, env:dict={}, imports=""):
     "Return a function that will setup os.environ and execute a target function within a context manager."
     if l: assert i < len(l), ValueError("Invalid index {i} > result list size: {len(l)}")
     def _cfn(*args, **kwargs):
         os.environ.update({"LOCAL_RANK":str(i), "LOCAL_WORLD_SIZE":str(nprocs)})
         try:
-            '''
-            The following snippet import modules into function 'fn()''s globals scope.
-
-            import inspect, importlib
-            try: g_member = next(filter(lambda t: t[0] == '__globals__', inspect.getmembers(fn)))
-            except StopIteration: raise KeyError("Wow what kind of object is fn?! no '__globals__'? ")
-            m = __import__('torch')
-
-            # g_member[1] is the same as "globals()" inside "fn()"
-            g_member[1]['torch'] = m
-            import_star(['numpy'], g_member[1])
-            g_member[1]['dist' ] = importlib.import_module('torch.distributed')
-
-            for o in objlist: g_member[1][o] = outside[o]
-            '''
-            import inspect, importlib
-            try: g_member = next(filter(lambda t: t[0] == '__globals__', inspect.getmembers(fn)))
-            except StopIteration: raise KeyError("Wow what kind of object is fn?! no '__globals__'? ")
-            # m = __import__('torch')
-            # g_member[1] is the same as "globals()" inside "fn()"
-            # g_member[1]['torch'] = m
-            # import_star(['numpy'], g_member[1])
-
-            for k,v in env.items(): g_member[1][k] = v # pass in explicit objects
+            import sys, mpify
+            # import things into the '__main__', which can be in a subprocess here.
+            g = sys.modules['__main__'].__dict__
+            mpify.global_imports(imports.split('\n'), g)
+            g.update(env)
 
             with cm or nullcontext(): r = fn(*args, **kwargs)
             if l: l[i] = r
@@ -75,9 +57,10 @@ def _contextualize(i:int, nprocs:int, fn:Callable, cm:AbstractContextManager, l=
         finally: map(lambda k: os.environ.pop(k, None), ("LOCAL_RANK", "LOCAL_WORLD_SIZE"))               
     return _cfn
 
-def ranch(nprocs:int, fn:Callable, *args, caller_rank:int=0, gather:bool=True, ctx=None, need:str=None, **kwargs):
+def ranch(nprocs:int, fn:Callable, *args, caller_rank:int=0, gather:bool=True, ctx=None, need:str="", imports="", **kwargs):
     '''Spawn `nprocs` ranked process and launch `fn(*args, **kwargs)`. Caller process can participate as `caller_rank`.
-    Apply ctx mgr if provided. `gather`: If True (default), return list of all function return values; otherwise return that executed in the parent rank process, and that requires `caller_rank` be defined.
+    Apply ctx mgr if provided.  If `gather` is True (default), return list of all function return values.
+    Otherwise if caller participates, return the value from its execution of fn, or None if caller doesn't participate.
     '''
     assert nprocs > 0, ValueError("nprocs: # of processes to launch must be > 0")
     
@@ -85,26 +68,18 @@ def ranch(nprocs:int, fn:Callable, *args, caller_rank:int=0, gather:bool=True, c
     if caller_rank is not None:
         assert 0 <= caller_rank < nprocs, ValueError(f"Invalid caller_rank {caller_rank}, must satisfy 0 <= caller_rank < {nprocs}")
         children_ranks.pop(caller_rank)
-
-    print(f"Spawning ranks: [{children_ranks}]")
-
     multiproc_ctx, procs = mp.get_context("spawn"), []
     result_list = multiproc_ctx.Manager().list([None] * nprocs) if gather else None
-    try: # First launch the execution in the subprocesses, then in current process if caller_rank is not None
-        env = {}
-        if need is not None:
-            cf = inspect.currentframe()
-            caller_g = cf.f_back.f_globals
-            objs = need.split()
-            for o in objs: env[o] = caller_g[o]
-            del cf
-
+    try:
+        # pass globals in this process to subprocess via fn's wrapper, 'target_fn'
+        env = {k : sys.modules['__main__'].__dict__[k] for k in need.split()}
+        print(f"Passing env to contextualize(): {env}")
         for rank in children_ranks:
-            target_fn = _contextualize(rank, nprocs, fn, cm=ctx, l=result_list, env=env)
+            target_fn = _contextualize(rank, nprocs, fn, cm=ctx, l=result_list, env=env, imports=imports)
             p = multiproc_ctx.Process(target=target_fn, args=args, kwargs=kwargs)
             procs.append(p)
             p.start()
-        p_res = (_contextualize(caller_rank, nprocs, fn, cm=ctx, l=result_list))(*args, **kwargs) if caller_rank is not None else None
+        p_res = (_contextualize(caller_rank, nprocs, fn, cm=ctx, l=result_list, env=env, imports=imports))(*args, **kwargs) if caller_rank is not None else None
         for p in procs: p.join()
         return result_list if gather else p_res
     finally:
@@ -121,7 +96,8 @@ class TorchDDPCtx(AbstractContextManager):
 
     def __enter__(self):
         try: local_rank, local_ws = int(os.environ['LOCAL_RANK']), int(os.environ['LOCAL_WORLD_SIZE'])
-        except KeyError as e: raise KeyError(f"os.environ['LOCAL_RANK'] and os.environ['LOCAL_RANK'] must be set prior to entering the context.") from e
+        except KeyError as e:
+            raise KeyError(f"os.environ['LOCAL_RANK'] and os.environ['LOCAL_RANK'] must be set when __enter__()ing.") from e
 
         assert 0 < local_ws <= self._ws, ValueError(f"Invalid os.environ['LOCAL_WORLD_SIZE']: {local_ws}, should be 0 < ws <= {self._ws}!")
 
@@ -132,8 +108,10 @@ class TorchDDPCtx(AbstractContextManager):
             try:
                 torch.cuda.set_device(local_rank)
                 self._backend = 'nccl'
-            except RuntimeError as e: self._use_gpu = False
-            print(f"Rank [{rank}] using CUDA GPU {local_rank}" if self._use_gpu else f"Failed to set CUDA device to {local_rank}. Invalid os.environ['LOCAL_RANK']?", file=sys.stderr, flush=True)
+                print(f"Rank [{rank}] using CUDA GPU {local_rank}", flush=True)
+            except RuntimeError as e:
+                self._use_gpu = False
+                print(f"Failed to set CUDA device to {local_rank}. Invalid os.environ['LOCAL_RANK']?  Available device count: {torch.cuda.device_count()}", file=sys.stderr, flush=True)
 
         os.environ.update({"WORLD_SIZE":str(self._ws), "RANK":str(rank),
             "MASTER_ADDR":self._a, "MASTER_PORT":self._p, "OMP_NUM_THREADS":self._nt})
@@ -151,9 +129,10 @@ class TorchDDPCtx(AbstractContextManager):
 def ddp_rank(): return int(os.environ['RANK'])
 def ddp_worldsize(): return int(os.environ['WORLD_SIZE'])
 
-def in_torchddp(nprocs:int, fn:Callable, *args, ctx:TorchDDPCtx=None, world_size:int=None, base_rank:int=0, need:str=None, **kwargs):
+def in_torchddp(nprocs:int, fn:Callable, *args, world_size:int=None, base_rank:int=0,
+                ctx:TorchDDPCtx=None, need:str="", imports:str="", **kwargs):
     "Launch `fn(*args, **kwargs)` in Torch DDP group of 'world_size' members on `nprocs` local processes RANK from ['base_rank'..'nprocs'-1]"
     if world_size is None: world_size = nprocs
     assert base_rank + nprocs <= world_size, ValueError(f"nprocs({nprocs}) + base_rank({base_rank}) must be < world_size({world_size})")
-    if ctx is None: ctx = TorchDDPCtx(world_size=world_size, base_rank=base_rank, need=need)
-    return ranch(nprocs, fn, *args, caller_rank=0, gather=False, ctx=ctx,  **kwargs)
+    if ctx is None: ctx = TorchDDPCtx(world_size=world_size, base_rank=base_rank)
+    return ranch(nprocs, fn, *args, caller_rank=0, gather=False, ctx=ctx, need=need, imports=imports, **kwargs)
